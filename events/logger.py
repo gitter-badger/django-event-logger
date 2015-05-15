08efd8c9b9502c4769685eaddaf7c0cdcb355166
Brandon B <unaffiliate@outlook.com>
2015-05-15 01:41:39 -0400

    Added the EventLogger class to ease logging

--- events/logger.py
+++ events/logger.py
@@ -1,8 +1,10 @@
 from events.models import Event
 
+
 class Events:
     def __init__(self):
         pass
+
     def log_event(self, username, action, response, additional='N/A'):
         try:
             log = Event(username=username, action=action, response=response, additional=additional)
