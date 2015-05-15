2dcd161fb8e6afb84485a7060ec40c2aa32da6d3
Brandon B <unaffiliate@outlook.com>
2015-05-15 01:50:50 -0400

    Add the setup.py file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name = "django-event-logger",
+    version = "0.5.0",
+    packages = find_packages(),
+
+    install_requires = ['Django'],
+
+    author = "Brandon B.",
+    author_email = "unaffiliate@outlook.com",
+    description = "An easy-to-use event logger for Django.",
+    keywords = "django events logger logging",
+)
